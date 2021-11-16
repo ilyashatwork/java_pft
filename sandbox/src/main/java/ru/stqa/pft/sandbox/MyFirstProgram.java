58f3e52fd355be4255049ea1b6400f9946b49e22
ilyashatwork <94391506+ilyashatwork@users.noreply.github.com>
2021-11-16 11:46:17 +0400

    Add homework № 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,13 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.Scanner;
+
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Point p = new Point(1, 1, 5, 5);
 
+        System.out.println("Расстояние между первой и второй точками = " + p.distance());
+    }
 }
